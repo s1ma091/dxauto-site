f1fe564f51ddfc099c57702d096ec87a7e78e5c0
s1ma091 <s1ma0@ukr.net>
2017-05-16 16:02:25 +0300

    add

--- common/config/main.php
+++ common/config/main.php
@@ -2,6 +2,13 @@
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost; dbname=dxauto_site',
+            'username' => 'u_dxauto',
+            'password' => 'zxd6ukxx',
+            'charset' => 'utf8',
+        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
